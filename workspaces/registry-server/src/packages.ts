851ad71f6af4475fbb5a483fe968ec1fb0212038
Laurence Roberts <lsjroberts@outlook.com>
2018-10-11 08:59:15 +0100

    Add routes for packages

--- workspaces/registry-server/src/packages.ts
+++ workspaces/registry-server/src/packages.ts
@@ -17,6 +17,30 @@ export function count() {
   return Object.keys(packages).length;
 }
 
+export function findVersions(user, name) {
+  const found = packages[path.join(user, name)];
+
+  if (!found) {
+    return [];
+  }
+
+  return found.versions || [];
+}
+
+export function find(user, name, version) {
+  const found = packages[path.join(user, name)];
+
+  if (!found) {
+    return false;
+  }
+
+  if (!found.versions.includes(version)) {
+    return false;
+  }
+
+  return true;
+}
+
 export async function refresh(dir) {
   const packagesPath = new URL(path.join(dir, 'packages.json'), registriesUrl);
   packages = await readFile(packagesPath).then(buffer =>
--- workspaces/registry-server/src/router.ts
+++ workspaces/registry-server/src/router.ts
@@ -1,12 +1,71 @@
-import { route, respond } from 'wool-server/router';
+import * as path from 'path';
+import { readFile, woolUrl } from 'wool/utils';
+import { header, route, status } from 'wool-server/router';
+
+import * as packages from './packages';
 
 export default [
-  route('/', () => [], () => 'Hello, World!'),
-  route('/packages', () => [], () => 'Packages'),
-  route(
-    '/packages/#{user}/#{name}/#{version}',
-    () => [],
-    ({ user, name, version }) =>
-      `Requesting package: ${user}/${name}/${version}`,
-  ),
+  route('/', () => 'Hello, World!'),
+  route('/packages', () => 'Packages'),
+  route('/packages/{user}/{name}', packageIndex),
+  route('/packages/{user}/{name}/{version}', packageVersion),
+  route('/packages/{user}/{name}/{version}/bundle', bundle),
 ];
+
+function packageIndex(params) {
+  const versions = packages.findVersions(params.user, params.name);
+
+  if (versions.length === 0) {
+    return {
+      attrs: [status(404)],
+      body: '',
+    };
+  }
+
+  return JSON.stringify({ versions });
+}
+
+function packageVersion(params) {
+  const found = packages.find(params.user, params.name, params.version);
+
+  if (!found) {
+    return {
+      attrs: [status(404)],
+      body: '',
+    };
+  }
+
+  // TODO: read package wool.json
+  return JSON.stringify({
+    name: `${params.user}/${params.name}`,
+    version: params.version,
+    entry: 'src/index.ts',
+    size: 1234,
+  });
+}
+
+async function bundle(params) {
+  const found = packages.find(params.user, params.name, params.version);
+
+  if (!found) {
+    return {
+      attrs: [status(404)],
+      body: '',
+    };
+  }
+
+  const bundleName = `${params.user}_${params.name}_${params.version}.tar.gz`;
+
+  return readFile(
+    new URL(path.join('registries', 'example', 'bundles', bundleName), woolUrl),
+  )
+    .then(data => ({
+      attrs: [header('Content-Type', 'application/gzip')],
+      body: data,
+    }))
+    .catch(err => ({
+      // This error should be 5xx, not a 404
+      attrs: [status(404)],
+      body: '',
+    }));
+}
